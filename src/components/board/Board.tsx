98006f0b2fcdea83886e4f23df29b29ceb74651d
Nizamph <116118003+Nizamph@users.noreply.github.com>
2023-12-13 14:04:46 +0530

    drawing functionalities added

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,14 +1,91 @@
-import React, { useEffect } from 'react'
-import { useRef } from 'react'
+import { RootState } from '@/reduxStore/store';
+import React, { useEffect, useState } from 'react';
+import { useRef } from 'react';
+import { useSelector } from 'react-redux';
+
+type properties = {
+  color: string;
+  size: string | undefined;
+};
+
 const Board = () => {
-    const currentRef = useRef<HTMLCanvasElement | null>(null)
+  const currentRef = useRef<HTMLCanvasElement | null>(null);
+  const shouldDraw = useRef<HTMLCanvasElement | boolean>(false);
+  const currentTabName: string = useSelector(
+    (store: RootState) => store.app.currentNavbarTool
+  );
 
-    useEffect(() => {
-        if (!currentRef.current) return
-        currentRef.current.width = window.innerWidth
-        currentRef.current.height = window.innerHeight
-    })
-    return <canvas ref={currentRef}></canvas>
-}
+  const pencilProperties: properties = useSelector(
+    (store: RootState) => store.toolbox.pencilProperty
+  );
+  const eraserProperties: properties = useSelector(
+    (store: RootState) => store.toolbox.eraserProperty
+  );
 
-export default Board
+  useEffect(() => {
+    if (!currentRef.current) return;
+    currentRef.current.width = window.innerWidth;
+    currentRef.current.height = window.innerHeight;
+
+    const canvas: HTMLCanvasElement | null = currentRef.current;
+    const context: CanvasRenderingContext2D | null = canvas?.getContext('2d');
+    const beginDraw = (x: number, y: number) => {
+      context?.beginPath();
+      context?.lineTo(x, y);
+    };
+
+    const drawLine = (x: number, y: number) => {
+      context?.lineTo(x, y);
+      context?.stroke();
+    };
+    const mouseDownHandler = (e: any) => {
+      shouldDraw.current = true;
+      if (shouldDraw.current) {
+        beginDraw(e.clientX, e.clientY);
+      }
+    };
+    const mouseMoveHandler = (e: any) => {
+      if (shouldDraw.current) {
+        drawLine(e.clientX, e.clientY);
+      }
+    };
+    const mouseUpHandler = (e: any) => {
+      shouldDraw.current = false;
+    };
+
+    canvas.addEventListener('mousedown', mouseDownHandler);
+    canvas.addEventListener('mousemove', mouseMoveHandler);
+    canvas.addEventListener('mouseup', mouseUpHandler);
+
+    return () => {
+      canvas.removeEventListener('mousedown', mouseDownHandler);
+      canvas.removeEventListener('mousemove', mouseMoveHandler);
+      canvas.removeEventListener('mouseup', mouseUpHandler);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!currentRef.current) return;
+
+    const canvas: HTMLCanvasElement | null = currentRef.current;
+    const context: CanvasRenderingContext2D | null = canvas?.getContext('2d');
+
+    const changeConfig = () => {
+      if (!context) return;
+
+      if (currentTabName === 'pencil') {
+        context.strokeStyle = pencilProperties.color;
+        context.lineWidth = Number(pencilProperties.size);
+      } else {
+        context.strokeStyle = eraserProperties.color;
+        context.lineWidth = Number(eraserProperties.size);
+      }
+    };
+
+    changeConfig();
+  }, [pencilProperties, eraserProperties, currentTabName]);
+
+  return <canvas ref={currentRef}></canvas>;
+};
+
+export default React.memo(Board);
--- src/reduxStore/appSlice.ts
+++ src/reduxStore/appSlice.ts
@@ -1,25 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { iconOptions } from '@/constants/Constants'
+import { createSlice } from '@reduxjs/toolkit';
+import { iconOptions } from '@/constants/Constants';
 const appSlice = createSlice({
-    name: 'app',
-    initialState: {
-        visibilityOfColorOptions: true,
-        currentNavbarTool: 'pencil',
+  name: 'app',
+  initialState: {
+    visibilityOfColorOptions: true,
+    currentNavbarTool: 'pencil',
+  },
+  reducers: {
+    changeColorOptionVisibility: (state, action) => {
+      action.payload === 'hideColorOptions'
+        ? (state.visibilityOfColorOptions = false)
+        : (state.visibilityOfColorOptions = true);
     },
-    reducers: {
-        changeColorOptionVisibility: (state, action) => {
-            action.payload === 'hideColorOptions'
-                ? (state.visibilityOfColorOptions = false)
-                : (state.visibilityOfColorOptions = true)
-        },
-        setCurrentNavbarTool: (state, action) => {
-            state.currentNavbarTool = action.payload.iconName
-            console.log(state.currentNavbarTool)
-        },
+    setCurrentNavbarTool: (state, action) => {
+      state.currentNavbarTool = action.payload.iconName;
+      console.log(state.currentNavbarTool);
     },
-})
+  },
+});
 
 export const { changeColorOptionVisibility, setCurrentNavbarTool } =
-    appSlice.actions
+  appSlice.actions;
 
-export default appSlice.reducer
+export default appSlice.reducer;
